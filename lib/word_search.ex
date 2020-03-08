aba1601fb4c115d3e512f65d607f1522e2c88b5a
Jarrett Baugh <jarrett.baugh@gmail.com>
2020-03-08 09:28:26 -0700

    Initial commit

---
+++ lib/word_search.ex
@@ -0,0 +1,18 @@
+defmodule WordSearch do
+  @moduledoc """
+  Documentation for `WordSearch`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> WordSearch.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/word_search_test.exs
@@ -0,0 +1,8 @@
+defmodule WordSearchTest do
+  use ExUnit.Case
+  doctest WordSearch
+
+  test "greets the world" do
+    assert WordSearch.hello() == :world
+  end
+end
