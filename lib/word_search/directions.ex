4e83032ec5fb5f7e4795cb83b3fc84146367e0ec
Jarrett Baugh <jarrett.baugh@gmail.com>
2020-03-10 12:03:29 -0700

    Orient word/direction with existing word

--- lib/word_search/directions.ex
+++ lib/word_search/directions.ex
@@ -1,10 +1,26 @@
 defmodule WordSearch.Directions do
+  def orient_word_and_direction(word, "left") do
+    {String.reverse(word), "right"}
+  end
+  def orient_word_and_direction(word, "up") do
+    {String.reverse(word), "down"}
+  end
+  def orient_word_and_direction(word, "leftup") do
+    {String.reverse(word), "rightdown"}
+  end
+  def orient_word_and_direction(word, "leftdown") do
+    {String.reverse(word), "rightup"}
+  end
+  def orient_word_and_direction(word, direction) do
+    {word, direction}
+  end
+
   def convert(directions) do
     []
     |> convert_directions(directions, ["forward"], ["right", "down"])
     |> convert_directions(directions, ["backward"], ["left", "up"])
-    |> convert_directions(directions, ["diagonal"], ["rightdown"])
-    |> convert_directions(directions, ["backward", "diagonal"], ["leftup", "leftdown", "rightup"])
+    |> convert_directions(directions, ["diagonal"], ["rightdown", "rightup"])
+    |> convert_directions(directions, ["backward", "diagonal"], ["leftup", "leftdown"])
   end
   defp convert_directions(acc, input_dirs, check_dirs, add_dirs) do
     if Enum.all?(check_dirs, fn x -> x in input_dirs end) do
--- test/word_search/directions_test.ex
+++ test/word_search/directions_test.ex
@@ -1,6 +1,26 @@
 defmodule WordSearch.DirectionsTest do
   use ExUnit.Case
 
+  test "reverses word and sets new direction for left" do
+    assert WordSearch.Directions.orient_word_and_direction("word", "left") == {"drow", "right"}
+  end
+  test "reverses word and sets new direction for up" do
+    assert WordSearch.Directions.orient_word_and_direction("word", "up") == {"drow", "down"}
+  end
+  test "reverses word and sets new direction for leftup" do
+    assert WordSearch.Directions.orient_word_and_direction("word", "leftup") == {"drow", "rightdown"}
+  end
+  test "reverses word and sets new direction for leftdown" do
+    assert WordSearch.Directions.orient_word_and_direction("word", "leftdown") == {"drow", "rightup"}
+  end
+  test "returns word and direction for anything else" do
+    assert WordSearch.Directions.orient_word_and_direction("word", "right") == {"word", "right"}
+    assert WordSearch.Directions.orient_word_and_direction("word", "down") == {"word", "down"}
+    assert WordSearch.Directions.orient_word_and_direction("word", "rightdown") == {"word", "rightdown"}
+    assert WordSearch.Directions.orient_word_and_direction("word", "rightup") == {"word", "rightup"}
+    assert WordSearch.Directions.orient_word_and_direction("word", "fooby") == {"word", "fooby"}
+  end
+
   test "converts forward input to placement directions" do
     assert WordSearch.Directions.convert(["forward"]) == ["right", "down"]
   end
@@ -8,12 +28,12 @@ defmodule WordSearch.DirectionsTest do
     assert WordSearch.Directions.convert(["backward"]) == ["left", "up"]
   end
   test "converts diagonal input to placement directions" do
-    assert WordSearch.Directions.convert(["diagonal"]) == ["rightdown"]
+    assert WordSearch.Directions.convert(["diagonal"]) == ["rightdown", "rightup"]
   end
   test "converts backward and diagonal input to placement directions" do
-    assert WordSearch.Directions.convert(["backward", "diagonal"]) == ["left", "up", "rightdown", "leftup", "leftdown", "rightup"]
+    assert WordSearch.Directions.convert(["backward", "diagonal"]) == ["left", "up", "rightdown", "rightup", "leftup", "leftdown"]
   end
   test "converts input for all to placement directions" do
-    assert WordSearch.Directions.convert(["forward", "backward", "diagonal"]) == ["right", "down", "left", "up", "rightdown", "leftup", "leftdown", "rightup"]
+    assert WordSearch.Directions.convert(["forward", "backward", "diagonal"]) == ["right", "down", "left", "up", "rightdown", "rightup", "leftup", "leftdown"]
   end
 end
