9d146d9d5a9718fa3cf42f0bdd5b5bfcbd605c36
Jarrett Baugh <jarrett.baugh@gmail.com>
2020-03-11 19:47:34 -0700

    Add some documentation

--- lib/word_search.ex
+++ lib/word_search.ex
@@ -20,6 +20,13 @@ defmodule WordSearch do
       F S H L G T H H H B
       B P V Y D O E F E Y
       D R E D L I R Z V R
+  
+
+      words - list of words to be used in the word search
+      size - size of word search (a size of 10 will generate a 10x10 word search)
+      difficulty - easy or hard. easy will use the entire alphabet for filling spaces, hard will only use characters from the input words
+      directions - list of directions the words can be placed: forward, diagonal and backward. combining diagonal and backward will allow backward-diagonal words
+      language - which character set to use. right now only english is supported, however words with special characters will be added to the alphabet for the word search 
   """
   def generate(words, size) do
     generate(words, size, "easy", ["forward", "diagonal"], "english")
